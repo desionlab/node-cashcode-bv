e1b22720515f014608faa5ef3e5da2969ad5a118
fenixphp <fenixphp@gmail.com>
2018-09-21 00:55:50 +0300

    Add CCNet const.

---
+++ build/Const/CCNet.js
@@ -0,0 +1,34 @@
+"use strict";
+/**
+ * CCNet.ts
+ *
+ * @author    Desionlab <fenixphp@gmail.com>
+ * @copyright 2017 - 2018 Desionlab
+ * @license   MIT
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Message transmission start code.
+ */
+exports.SYNC = 0x02;
+/**
+ * Poly for CRC calculated.
+ */
+exports.CRC_POLY = 0x08408;
+/**
+ * Bill to bill peripheral addresses.
+ */
+exports.ADR_BILL_TO_BILL = 0x01;
+/**
+ * Coin changer peripheral addresses.
+ */
+exports.ADR_COIN_CHANGER = 0x02;
+/**
+ * Bill validator peripheral addresses.
+ */
+exports.ADR_BILL_VALIDATOR = 0x03;
+/**
+ * Card reader peripheral addresses.
+ */
+exports.ADR_CARD_READER = 0x04;
+/* End of file CCNet.ts */ 
