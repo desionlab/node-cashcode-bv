f69834f2a8c40b8e25ef200802cfcb79e1fe9e47
fenixphp <fenixphp@gmail.com>
2018-09-23 20:23:45 +0300

    [WIP] Basic classes.

--- src/Command.ts
+++ src/Command.ts
@@ -6,6 +6,37 @@
  * @license   MIT
  */
 
+/**
+ * Class Command
+ * 
+ * An abstract class of device command logic.
+ * 
+ * @version 1.0.0
+ */
+export abstract class Command {
 
+  public constructor () {
+    
+  }
+
+  public request () {
+
+  }
+
+  /**
+   * Processing command response.
+   * 
+   * @param data Data received from the device.
+   * @returns { any } The processed data are presented in a convenient form.
+   */
+  public response (data: Buffer) {
+
+  }
+
+  protected assemble () {
+
+  }
+
+}
 
 /* End of file Command.ts */
