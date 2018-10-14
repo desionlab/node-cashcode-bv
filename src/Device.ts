a6d98db472779143f3bbc12e3dc04f1166878e95
fenixphp <fenixphp@gmail.com>
2018-10-14 04:16:59 +0300

    [WIP] Basic classes.

--- src/Device.ts
+++ src/Device.ts
@@ -17,43 +17,59 @@ import { Exception } from './Exception';
 import { getCRC16 } from './Utils';
 
 /**
- * 
+ * Indicates state of the Bill Validator and its activity.
  */
 export const enum DeviceStatus {
-  POWER_UP,
-  POWER_UP_WITH_BILL_IN_VALIDATOR,
-  POWER_UP_WITH_BILL_IN_STACKER,
+  POWER_UP                                                   = '10',
+  POWER_UP_WITH_BILL_IN_VALIDATOR                            = '11',
+  POWER_UP_WITH_BILL_IN_STACKER                              = '12',
   /* ----------------------------------------------------------------------- */
-  INITIALIZE,
-  IDLING,
-  ACCEPTING,
-  STACKING,
-  RETURNING,
-  UNIT_DISABLED,
-  HOLDING,
-  DEVICE_BUSY,
+  INITIALIZE                                                 = '13',
+  IDLING                                                     = '14',
+  ACCEPTING                                                  = '15',
+  STACKING                                                   = '17',
+  RETURNING                                                  = '18',
+  UNIT_DISABLED                                              = '19',
+  HOLDING                                                    = '1a',
+  DEVICE_BUSY                                                = '1b',
   /* ----------------------------------------------------------------------- */
-
+  REJECTING_DUE_TO_INSERTION                                 = '1c60',
+  REJECTING_DUE_TO_MAGNETIC                                  = '1c61',
+  REJECTING_DUE_TO_REMAINED_BILL_IN_HEAD                     = '1c62',
+  REJECTING_DUE_TO_MULTIPLYING                               = '1c63',
+  REJECTING_DUE_TO_CONVEYING                                 = '1c64',
+  REJECTING_DUE_TO_IDENTIFICATION                            = '1c65',
+  REJECTING_DUE_TO_VERIFICATION                              = '1c66',
+  REJECTING_DUE_TO_OPTIC                                     = '1c67',
+  REJECTING_DUE_TO_INHIBIT                                   = '1c68',
+  REJECTING_DUE_TO_CAPACITY                                  = '1c69',
+  REJECTING_DUE_TO_OPERATION                                 = '1c6a',
+  REJECTING_DUE_TO_LENGTH                                    = '1c6c',
+  REJECTING_DUE_TO_UNRECOGNISED_BARCODE                      = '1c92',
+  REJECTING_DUE_TO_UV                                        = '1c6d',
+  REJECTING_DUE_TO_INCORRECT_NUMBER_OF_CHARACTERS_IN_BARCODE = '1c93',
+  REJECTING_DUE_TO_UNKNOWN_BARCODE_START_SEQUENCE            = '1c94',
+  REJECTING_DUE_TO_UNKNOWN_BARCODE_STOP_SEQUENCE             = '1c95',
   /* ----------------------------------------------------------------------- */
-  DROP_CASSETTE_FULL,
-  DROP_CASSETTE_OUT_OF_POSITION,
-  VALIDATOR_JAMMED,
-  DROP_CASSETTE_JAMMED,
-  CHEATED,
-  PAUSE,
+  DROP_CASSETTE_FULL                                         = '41',
+  DROP_CASSETTE_OUT_OF_POSITION                              = '42',
+  VALIDATOR_JAMMED                                           = '43',
+  DROP_CASSETTE_JAMMED                                       = '44',
+  CHEATED                                                    = '45',
+  PAUSE                                                      = '46',
   /* ----------------------------------------------------------------------- */
-  STACK_MOTOR_FAILURE,
-  TRANSPORT_MOTOR_SPEED_FAILURE,
-  TRANSPORT_MOTOR_FAILURE,
-  ALIGNING_MOTOR_FAILURE,
-  INITIAL_CASSETTE_STATUS_FAILURE,
-  OPTIC_CANAL_FAILURE,
-  MAGNETIC_CANAL_FAILURE,
-  CAPACITANCE_CANAL_FAILURE,
+  STACK_MOTOR_FAILURE                                        = '4750',
+  TRANSPORT_MOTOR_SPEED_FAILURE                              = '4751',
+  TRANSPORT_MOTOR_FAILURE                                    = '4752',
+  ALIGNING_MOTOR_FAILURE                                     = '4753',
+  INITIAL_CASSETTE_STATUS_FAILURE                            = '4754',
+  OPTIC_CANAL_FAILURE                                        = '4755',
+  MAGNETIC_CANAL_FAILURE                                     = '4756',
+  CAPACITANCE_CANAL_FAILURE                                  = '475f',
   /* ----------------------------------------------------------------------- */
-  ESCROW_POSITION,
-  BILL_STACKED,
-  BILL_RETURNED
+  ESCROW_POSITION                                            = '80',
+  BILL_STACKED                                               = '81',
+  BILL_RETURNED                                              = '82'
 };
 
 /**
